5d8b0c390f1d540eb0b35f4e95ff12ebd14b0bb5
Arun Govind M <arungovindm@gmail.com>
2021-04-18 14:06:58 +0530

    Displays current temp, pressure, humidity and description

---
+++ README.md
@@ -0,0 +1,51 @@
+# Weather App
+
+A simple weather app using Flask and OpenWeather API
+
+
+# Installation
+
+## For macOS/Linux
+Clone the repositiory using this command
+```
+    git clone https://github.com/arungovindm2001/weather-app.git
+```
+Install a virtual environment
+```
+    pip3 install virtualenv
+```
+Activate the virtual environment
+```
+    virtualenv env
+    source env/bin/activate
+```
+Install the dependencies
+```
+    pip3 install -r requirements.txt
+```
+Run the app
+```
+    python3 app.py
+```
+## For Windows
+Clone the repositiory using this command
+```
+    git clone https://github.com/arungovindm2001/weather-app.git
+```
+Install a virtual environment
+```
+    pip install virtualenv
+```
+Activate the virtual environment
+```
+    virtualenv env
+    \path\to\env\Scripts\activate
+```
+Install the dependencies
+```
+    pip install -r requirements.txt
+```
+Run the app
+```
+    python app.py
+ ```
---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import requests
+import json
+
+app = Flask(__name__)
+app.secret_key = "hello"
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/city", methods=["POST", "GET"])
+def city():
+    if request.method == "POST":
+        cityname = request.form["city"]
+        api_key = "d2545936ebf382f3e65dc6d346b6fac2"
+
+        base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+        complete_url = base_url + "q=" + cityname + "&appid=" + api_key
+
+        response = requests.get(complete_url)
+        x = response.json()
+
+        if x["cod"] != "404":
+            y = x["main"]
+            currentTemperature = y["temp"]
+            current_pressure = y["pressure"]
+            current_humidiy = y["humidity"]
+            z = x["weather"]
+            weather_description = z[0]["description"]
+            return(
+                " Temperature : " + str(currentTemperature - 273.15) + u"\N{DEGREE SIGN}" + "C" + "<br>" +
+                "Atmospheric pressure : " + str(current_pressure) + " hPa" + "<br>" +
+                "Humidity : " + str(current_humidiy) + "%" + "<br>" +
+                "Description : " + str(weather_description)
+            )
+        else:
+            return "City Not Found"
+    else:
+        return render_template("city.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
